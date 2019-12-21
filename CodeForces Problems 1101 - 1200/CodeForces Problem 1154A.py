92993600e76f7ac1564dba00b8bef3351a7e0771
Sharvil Kesarwani <sharkesa@gmail.com>
2019-12-21 22:22:54 +1100

    Delete CodeForces Problem 1154A.py

--- CodeForces Problems 1101 - 1200/CodeForces Problem 1154A.py
+++
@@ -1,8 +0,0 @@
-numbers = [int(i) for i in input().split()]
-big = max(numbers)
-output = []
-for number in numbers:
-    if number != big:
-        output.append(str(big - number))
-
-print(' '.join(output))
